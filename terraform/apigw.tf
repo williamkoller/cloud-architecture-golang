8a42a5349a92b204e1c0764797df385f3dd6f03b
weslley ribeiro <wess_ribeiro@hotmail.com>
2025-08-08 12:19:46 -0300

    Update apigw.tf

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -5,11 +5,11 @@ resource "aws_apigatewayv2_api" "http_api" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id                 = aws_apigatewayv2_api.http_api.id
-  integration_type       = "AWS_PROXY"
-  integration_uri        = aws_lambda_function.golang_lambda.invoke_arn
-  payload_format_version = "2.0"
-  integration_method     = "POST"
+  api_id                  = aws_apigatewayv2_api.http_api.id
+  integration_type        = "AWS_PROXY"
+  integration_uri         = aws_lambda_function.golang_lambda.invoke_arn
+  payload_format_version  = "2.0"
+  integration_method      = "POST"
 }
 
 resource "aws_apigatewayv2_route" "health_route" {
@@ -22,4 +22,12 @@ resource "aws_apigatewayv2_stage" "default_stage" {
   api_id      = aws_apigatewayv2_api.http_api.id
   name        = "$default"
   auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.golang_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
