cb5ffe871ae19d011113b00ede90ca2ff9ea47c0
Weslley Ribeiro <weslley.ribeiro@eletromidia.com.br>
2025-08-08 10:49:37 -0300

    organizando recursos em arquivos separados para melhor leitura

--- infra/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "lambda_function_name" {
-  value = aws_lambda_function.golang_lambda.function_name
-}
---
+++ terraform/iam.tf
@@ -0,0 +1,20 @@
+# ===== IAM =====
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.env}-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+
+  depends_on = [aws_iam_role.lambda_exec]
+}
